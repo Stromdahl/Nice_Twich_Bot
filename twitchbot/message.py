78b7a8f4681b1a063acf0ebddcc0fef293f2b62a
Mattias Strömdahl <Mattias.stromdahl@gmail.com>
2021-08-10 12:00:19 +0200

    added message factory

--- twitchbot/message.py
+++ twitchbot/message.py
@@ -1,3 +1,5 @@
+from abc import ABC
+import re
 class Message:
     raw: str
     user: str
@@ -19,3 +21,48 @@ class Message:
 
     def __repr__(self) -> str:
         return f'{self.raw}'
+
+class MessageFactory:
+    """
+    Factory that is responsible for parsing raw string and create message objects
+    The Factory doesn't mainain any of the instaces it creates.
+    """
+    
+    def get_message(self, raw_message) -> Message: 
+        result = re.findall(pattern=r':tmi.twitch.tv (\d{3}) (\S+) :(.+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, irc_command=result[0], channel=result[1], text=result[2])
+            return message
+
+        result = re.findall(pattern=r':(\S+).tmi.twitch.tv (\d{3}) \1 (\S+) :(.+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, user=result[0], irc_command=result[1], channel=result[2], text=result[3])
+            return message
+
+        result = re.findall(pattern=r':(\S+).tmi.twitch.tv (\d{3}) \1 = (\S+) :(.+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, user=result[0], irc_command=result[1], channel=result[2], text=result[3])
+            return message
+
+        result = re.findall(pattern=r':(\S+)!\1@\1.tmi.twitch.tv (\S+) (\S+) :(.+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, user=result[0], irc_command=result[1], channel=result[2], text=result[3])
+            return message
+
+        result = re.findall(pattern=r':(\S+)!\1@\1.tmi.twitch.tv (\S+) (\S+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, user=result[0], irc_command=result[1], channel=result[2])
+            return message
+
+        result = re.findall(pattern=r'(\S+) :(.+)', string=raw_message)
+        if result:
+            result = result[0]
+            message = Message(raw_message, irc_command=result[0], text=result[1])
+            return message
+
+        return None
